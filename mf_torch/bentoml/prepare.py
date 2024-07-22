679d26c227380f4a4877008e106871966941785a
YuXuan Tay <wyextay@gmail.com>
2024-07-22 14:25:56 +0800

    feat: ray lightning cli (#74)
    * feat: ray lightning cli
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- mf_torch/bentoml/prepare.py
+++ mf_torch/bentoml/prepare.py
@@ -18,10 +18,10 @@ from mf_torch.params import (
 if TYPE_CHECKING:
     import lancedb.table
 
-    from mf_torch.lightning import LitMatrixFactorization
+    from mf_torch.lightning import MatrixFactorizationLitModule
 
 
-def prepare_model() -> LitMatrixFactorization:
+def prepare_model() -> MatrixFactorizationLitModule:
     from mf_torch.lightning import cli_main
 
     cli = cli_main(["fit"])
@@ -53,7 +53,7 @@ def prepare_movies() -> DocList[MovieCandidate]:
 
 
 def embed_movies(
-    movies: DocList[MovieCandidate], model: LitMatrixFactorization
+    movies: DocList[MovieCandidate], model: MatrixFactorizationLitModule
 ) -> DocList[MovieCandidate]:
     with torch.inference_mode():
         feature_hashes = torch.nested.nested_tensor(
@@ -93,7 +93,9 @@ def prepare_index(movies: DocList[MovieCandidate]) -> lancedb.table.LanceTable:
     return table
 
 
-def save_model(movies: DocList[MovieCandidate], model: LitMatrixFactorization) -> None:
+def save_model(
+    movies: DocList[MovieCandidate], model: MatrixFactorizationLitModule
+) -> None:
     import shutil
 
     import bentoml
