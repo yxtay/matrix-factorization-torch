9b05da6bc833d688d126ec31efcbeb03aea8c629
yxtay <5795122+yxtay@users.noreply.github.com>
2025-05-27 23:58:41 +0800

    Update prepare.py

--- mf_torch/bentoml/prepare.py
+++ mf_torch/bentoml/prepare.py
@@ -49,7 +49,7 @@ def prepare_trainer(
     from mf_torch.lightning import cli_main
 
     if ckpt_path is None:
-        args = {"trainer": {"fast_dev_run": True}, "data": {"batch_size": 4}}
+        args = {"trainer": {"fast_dev_run": True}}
         return cli_main({"fit": args}).trainer
 
     with tempfile.TemporaryDirectory() as tmp:
