b46a57c2cd2a6fb55864642c9db8393dad355561
YuXuan Tay <5795122+yxtay@users.noreply.github.com>
2025-05-28 10:36:56 +0800

    fix: remove-kics-ignore (#405)

--- mf_torch/data/prepare.py
+++ mf_torch/data/prepare.py
@@ -7,10 +7,7 @@ from concurrent.futures import ThreadPoolExecutor
 import polars as pl
 from loguru import logger
 
-from mf_torch.params import (
-    DATA_DIR,
-    MOVIELENS_1M_URL,
-)
+from mf_torch.params import DATA_DIR, MOVIELENS_1M_URL
 
 ###
 # download data
